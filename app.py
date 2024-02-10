ae1b3e440eed0c812f6d495d75d6ab2cbf167f6c
tranquilo <sanbonzakurakogyoshi@gmail.com>
2024-02-10 16:59:34 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from game_of_life import GameOfLife
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    GameOfLife(25, 25)
+    return render_template('index.html')
+
+
+@app.route('/live')
+def live():
+    game = GameOfLife()
+    game.form_new_generation()
+    return render_template('live.html', game=game)
+
+
+if __name__ == '__main__':
+    app.run()
